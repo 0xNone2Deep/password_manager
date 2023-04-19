2687c7510e0054296b547ae9f89bf7cc5dfc5156
0xNone2Deep <none2deep@proton.me>
2023-04-19 14:43:35 +0200

    Fixed Current Working Directory errors occuring when running the program from another location using terminal aliases

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from cryptography.fernet import Fernet
 from datetime import datetime as dt
 from getpass import getpass
+import os
+
+
 
 master_key = '0xNone2Deep'
 
-with open('key.key', 'rb') as key_file:
+with open(f'{os.getcwd()}/key.key', 'rb') as key_file:
     encr_key = key_file.read()
 
 
 def view_pass(key):
-    with open('passlist.txt', 'rb') as pl:
+    with open(f'{os.getcwd()}/passlist.txt', 'rb') as pl:
         passwords = pl.readlines()
         if not passwords:
             print('\nSorry, you do not have any saved passwords. Add a password first. \n')
@@ -29,7 +32,7 @@ def add_pass(key):
     fer = Fernet(key)
     encr_passw = fer.encrypt(passw.encode())
 
-    with open('passlist.txt', 'a') as pl:
+    with open(f'{os.getcwd()}/passlist.txt', 'a') as pl:
         pl.write(f'{dt.now()}|{account_id}|{encr_passw.decode()}\n')
     
     print('Password saved successfully! \n')
